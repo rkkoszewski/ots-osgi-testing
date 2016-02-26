419a675578f44efaf63d0bb7071d0924e3bf0982
rkkoszewski <rkkoszewski@gmail.com>
2016-02-26 22:56:59 +0100

    Main thread and Splash Service working - How to fix order dependency?

--- com.robertkoszewski.ots/src/com/robertkoszewski/utils/splashscreen/SimpleSplashScreenManager.java
+++ com.robertkoszewski.ots/src/com/robertkoszewski/utils/splashscreen/SimpleSplashScreenManager.java
@@ -1,11 +1,28 @@
 package com.robertkoszewski.utils.splashscreen;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 
 import com.robertkoszewski.utils.splashscreen.api.SplashScreenManager;
 
+
 @Component
 public class SimpleSplashScreenManager implements SplashScreenManager{
+
+	@Activate
+	public void activate(BundleContext ctx) {
+		System.out.println("@ Registering Splash Service");
+		ctx.registerService(
+				SplashScreenManager.class.getName(), 
+				new SimpleSplashScreenManager(), null);
+	}
+	
+	@Deactivate
+	public void deactivate(BundleContext ctx) {
+		System.out.println("@ UNRegistering Splash Service");
+	}
 
 	@Override
 	public void updateSplashMessage(String message) {
